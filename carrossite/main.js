87bbe24601a2c461cc01a73be85d45dc2f81e552
Gabriel-M4rtins <120607446+Gabriel-M4rtins@users.noreply.github.com>
2023-09-21 22:54:18 -0300

    Add files via upload

--- carrossite/main.js
+++ carrossite/main.js
@@ -1,9 +1,9 @@
 document.getElementById('next').onclick = function () {
-  const widthItem = document.querySelectorAll('.item').offsetWidth;
+  const widthItem = document.querySelector('.item').offsetWidth;
   document.getElementById('formList').scrollLeft += widthItem;
 }
 
 document.getElementById('prev').onclick = function () {
-  const widthItem = document.querySelectorAll('.item').offsetWidth;
+  const widthItem = document.querySelector('.item').offsetWidth;
   document.getElementById('formList').scrollLeft -= widthItem;
 }
