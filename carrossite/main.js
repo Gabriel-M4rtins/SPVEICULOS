facfcccfdc2c6bbfd1e8fbad92aaf1d9a45ad328
Gabriel-M4rtins <120607446+Gabriel-M4rtins@users.noreply.github.com>
2023-10-03 22:29:37 -0300

    Add files via upload

--- carrossite/main.js
+++ carrossite/main.js
@@ -7,3 +7,17 @@ document.getElementById('prev').onclick = function () {
   const widthItem = document.querySelector('.item').offsetWidth;
   document.getElementById('formList').scrollLeft -= widthItem;
 }
+
+
+const toggleBtn = document.querySelector('toggle-btn')
+const toggleBtnIcon = document.querySelector('toggle-btn i')
+const dropDownMenu = document.querySelector('dropdown-menu')
+
+toggleBtn.onclick = function () {
+  dropDownMenu.classList.toggle('open')
+  const isOpen = dropDownMenu.classList.contains('open')
+
+  toggleBtnIcon.classList = isOpen
+  ? 'fa-sloid fa-xmark'
+  : 'fa-solid fa-bars'  
+}
